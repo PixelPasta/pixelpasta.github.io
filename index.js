721f726fe05613ddf9977c2aede2807f17c50f26
PixelPasta <rachitjaiswal2@gmail.com>
2022-02-15 22:29:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let query
 
 window.addEventListener('load', (event) => {
     query = window.location.href
-    console.log(query.replace("http://127.0.0.1:5500/spongebob.html?query=", ""))
+    console.log(query.replace("http://127.0.0.1:5500/spongebob.html?query=", ''))
     query = query.replace("http://127.0.0.1:5500/spongebob.html?query=", "")
     window.location.href = `https://85ca9b45fbb4.up.railway.app/image/spongebobburn/?text=${query}`;
 });
