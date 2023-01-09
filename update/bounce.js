5983e9bb37a3c8c70cc38b4d2089960ec254fdbc
Pixel Pasta <80410384+PixelPasta@users.noreply.github.com>
2023-01-09 11:43:34 +0530

    Fixed links

--- update/bounce.js
+++ update/bounce.js
@@ -4,6 +4,15 @@ jQuery(function($) {
         if ($(this).is(':animated')) {return;}
             $(this).animate({ bottom: "+=60" }, {duration: 120, easing: "easeOutQuart"})
             .animate({ bottom: "-=60" }, {duration: 150, easing: "easeInSine"});
-            console.log(`h`)
+           
         });
+        $('.card2').mouseover(function() {
+            if ($(this).is(':animated')) {return;}
+                $(this).animate({ bottom: "+=60" }, {duration: 120, easing: "easeOutQuart"})
+                .animate({ bottom: "-=60" }, {duration: 150, easing: "easeInSine"});
+               
+            });
     });
+
+
+
